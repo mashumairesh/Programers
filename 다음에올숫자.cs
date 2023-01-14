6a74e3aae59eeb8f6e69db9a0f93179baadb2a7a
mashumairesh <dragon123764@gmail.com>
2023-01-14 20:12:46 +0900

    solved: 다음에 올 숫자

---
+++ 다음에올숫자.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Diagnostics;
+
+/// <summary>
+/// ExampleLink
+/// https://school.programmers.co.kr/learn/courses/30/lessons/120956
+/// </summary>
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution tmp = new Solution();
+        int[] tmp2 = new int[] { 1, 2, 3, 4 };
+        tmp.solution(tmp2);
+    }
+}
+
+
+public class Solution
+{
+    public int solution(int[] common)
+    {
+        int answer = 0;
+
+        //등차수열
+        //등비수열
+
+        bool isAP = false;
+        int Gab = 0, GabBefore = 0;
+
+        //등차수열과 등비수열 을 구분해야함.
+        for (int i = 0; i < common.Length - 1; i++)
+        {
+            Gab = common[i + 1] - common[i];
+
+            //등차
+            if (Gab == GabBefore)
+                isAP = true;
+            else
+                isAP = false;
+
+            GabBefore = Gab;
+        }
+
+        if (isAP)
+            answer = common[common.Length - 1] + Gab;
+        else
+        {
+            answer = common[common.Length - 1] * (common[2] / common[1]);
+        }
+        return answer;
+    }
+}
